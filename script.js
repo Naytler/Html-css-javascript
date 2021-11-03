f50688ec1fbe28cba9d216b94afcb13796abbc94
Sagirov Ruslan <justrus93@mail.ru>
2021-11-03 13:33:56 +0300

    2days writing

---
+++ script.js
@@ -0,0 +1,26 @@
+//first comment
+/*second 
+comment */
+// типы данных undefined, null, boolean, string, symbol, bigint, number and object
+// переменные создаются с var могут состоять из цифр букв $_ не могут начинться с цифр
+var myName;// создали переменную
+myName = 5;// = оператор присваивания присвоили 5 переменной myName
+var myNum //создали переменную myNum
+myNum = myName;//  и присвоили значение 5
+var myNew = 7; //обычно так присходит созданние переменной
+var myString = "строковый литерал";
+
+/*var a = 5;
+var b = 10;
+var c = "I am a";
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";*/
+let ;// создание переменных в ES6  разница в var и let in let покажет ошщбку если будешь перезаписывать переменные в течении кода
+
+let fix_salam = 1;
+console.log(fix_salam)
+
+
+
