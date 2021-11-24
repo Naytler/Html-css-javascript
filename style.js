72c6134c84ce07fdac52f75e4307b563e8542c96
Ruslan <93032282+Naytler@users.noreply.github.com>
2021-11-24 20:47:43 +0300

    js

--- style.js
+++ style.js
@@ -229,7 +229,7 @@ function trueOrFaalse(){
 console.log(trueOrFaalse(true));*/
 
 /*
-function boo(){
+function boo(num){
 if (num > 5 && num < 10){
     return "yes";
 }
@@ -244,3 +244,202 @@ function beetvin(val){
     }
     return "Inside";
 }*/
+
+
+/*
+function Test(num){
+    if(num < 5){
+        return "Tiny";
+    }else if(num < 10){
+        return "Small";
+    }else if(num < 15){
+        return "Medium";
+    }else if (num < 20){
+        return "Large"
+    }else{
+        return "Huge";
+    }
+}
+
+console.log(Test(11));*/
+
+/*
+const names = ["Hole-in-one", "eagle", "birdie", "par", "bogey","Double Bogey", "Go Home"];
+function tst(par, stockes){
+    if(stockes == 1){
+        return names[0];
+    }else if(stockes <= par - 2){
+        return names[1];
+    }else if(stockes == par - 1){
+        return names[2];
+    }else if(stockes == par){
+        return names[3];
+    }else if(stockes == par + 1){
+        return names[4];
+    }else if(stockes == par + 2){
+        return names [5];
+    }else{
+        return names[6];
+    }
+}
+
+console.log(tst(3,2))*/
+
+
+/*
+function polister(val){
+    let answer = "";
+    switch(val){
+        case 1: answer = ("alpha");
+        break;
+        case 2: answer = ("beta");
+        break;
+        case 3: answer = ("gamma");
+        break;
+        case 4: answer = ("delta");
+        break;
+    }
+    return answer;
+}
+
+console.log(polister(1));
+*/
+
+
+/*
+function tbt(val){
+    let answer = "";
+    switch(val){
+        case "a": answer = ("apple");
+        break;
+        case "b": answer = ("bird");
+        break;
+        case "c": answer = ("cat");
+        break;
+        default: answer = ("stuff");
+        break;
+    }
+    return answer;
+}
+
+console.log(tbt("a")); */
+
+/*
+function pott(val){
+    let answer = "";
+    switch(val){
+        case 1:
+        case 2:
+        case 3: answer = "1, 2, 3";
+        break;
+        case 4:
+        case 5:
+        case 6: answer = "4, 5, 6";
+        break;
+        default: answer = "another number";
+        break;
+    }
+    return answer;
+}
+console.log(pott(9));
+*/
+
+/*
+function sravnenie(a, b){
+    return a < b;
+}
+
+console.log(sravnenie(1, 2));*/
+
+
+
+/*
+let count
+function cc(card){
+    switch(card){
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6: count++
+        break;
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A": count--
+        break;
+    }
+    if(count <= 0){
+        return card + " " + "Hold"; 
+    }else{
+        return card + " " + "Bet";
+    }
+}
+
+console.log(cc(5));*/
+
+/*
+const myDog = {
+    "name": "Tailor",
+    "legs": 4,
+    "tails": 1,
+    "Friends": ["Human", "Dog"]
+};
+*/
+
+/*
+const testObj = {
+    "hat": "panama",
+    "shirt": "rubashka",
+    "shoes": "obuv"
+}
+
+let an = testObj.hat;
+let un = testObj["hat"];
+let zn = testObj.shirt;
+let xn = testObj["shirt"];
+console.log(an, un, zn, xn);*/
+
+
+/*
+const dog = {
+    "name": "lucky",
+    "age": 5,
+    "friends": "all"
+}
+
+dog.name = "Happy";
+delete dog["name"];
+console.log(dog.name);
+*/
+
+/*
+const alpha = {
+    1:"a",
+    2:"b",
+    3:"c",
+    4:"d",
+    24:"x",
+    25:"y",
+}
+
+const xer = prompt("напиши цифру для положения ее в алфавите");
+console.log(alpha[xer]);*/
+
+
+/*
+function phone(val){
+    let result = "";
+    const lookup = {
+        1: "adam",
+        2: "boston",
+        3: "charlie",
+        4: "delta",
+        5: "echo",
+    };
+    result = lookup[val];
+    return result;
+}
+
+console.log(phone(1));*/
